b1f603f3314cc6b9bd5d0e14702477cff6ecba8a
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-01-12 16:37:16 -0500

    dynamically pull in env vars

--- src/python/utilities/rest_api_pipeline.py
+++ src/python/utilities/rest_api_pipeline.py
@@ -5,8 +5,40 @@ from utilities.logger import logger
 
 ##########
 
+POSTGRES_ENV_MAP = {
+    "database": "DB",
+    "username": "USER",
+    "password": "PASSWORD",
+    "host": "HOST",
+    "port": "PORT",
+}
+
+
+def setup_credentials():
+    """
+    Loops through production environment variables
+    and assigns them to the expected dlt.secret variable
+    """
+
+    # This is the base credential string that we'll append to
+    secret_base = "citi_bike_prod.destination.postgres.credentials"
+
+    for dlt_var, env_var in POSTGRES_ENV_MAP.items():
+        # Attach expected variable to the credential string
+        # based on the Postgres variables in the environment
+        dlt_secret__key = f"{secret_base}.{dlt_var}"
+        env_var__key = f"DBT_PG_{env_var}__PROD"
+
+        dlt.secrets[dlt_secret__key] = os.environ[env_var__key]
+
+        logger.debug(f"Setting {dlt_secret__key} with {env_var__key} ENV VAR")
+
+    dlt.secrets[f"{secret_base}.connect_timeout"] = 90
+
+
 if os.environ.get("PROD") == "true":
     PIPELINE_NAME = "citi_bike_prod"
+    setup_credentials()
 else:
     PIPELINE_NAME = "citi_bike_dev"
 
