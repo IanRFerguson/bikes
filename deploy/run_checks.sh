5e1fb9689d59243ee2f04a24db106afd2a68b4ea
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-01-12 16:35:43 -0500

    use the correct db

--- deploy/run_checks.sh
+++ deploy/run_checks.sh
@@ -2,18 +2,9 @@
 
 set -e
 
-SECRETS_TARGET="./.dlt/secrets.toml"
-RAISE=false
-
-# Check for secrets.toml
-if [[ ! -f $SECRETS_TARGET  ]]; then
-    cp ./.dlt/secets.toml.template $SECRETS_TARGET
-    printf "\nCreated secrets.toml file in .dlt folder...\n\n"
-fi
-
 # Check for local env file
 if [[ ! -f local.env ]]; then
     cp ./local.env.template ./local.env
     printf "\nlocal.env file created - fill this out before running!\n\n"
-    RAISE=true
+    exit 1
 fi
